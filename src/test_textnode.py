d21306bc811e081440f0feb2273b1eae786c75c4
Zeriz64 <dark_ghoul16@yahoo.com>
2024-09-25 11:30:35 -0400

    Text node to html node and test made

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,6 +8,17 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    
+    def test_methods(self):
+        node_list = []
+        node_list.append(TextNode("This is Text Node", "text"))
+        node_list.append(TextNode("This is Bold Node", "bold"))
+        node_list.append(TextNode("This is Italic Node", "italic"))
+        node_list.append(TextNode("This is Code Node", "code"))
+        node_list.append(TextNode("This is link Node", "link", "link.url"))
+        node_list.append(TextNode("This is Image Node", "image", "image.url"))
+        for node in node_list:
+            print(text_node_to_html_node(node))
 
 
 if __name__ == "__main__":
