a35f8dc7d3a0686e8c5c8a1ddb782052c5d190ba
Zeriz64 <dark_ghoul16@yahoo.com>
2024-10-02 10:58:44 -0400

    working static_to_public

--- src/static_to_public.py
+++ src/static_to_public.py
@@ -1,19 +1,17 @@
 import os
 import shutil
 
-def static_to_public(static="/home/zeriz/workspace/course_projects/static_site/static", public="/home/zeriz/workspace/course_projects/static_site/public"):
-    if not os.path.exists(static):
-        raise Exception(f"{static} does not exist.")
+def static_to_public(static_path="/home/zeriz/workspace/course_projects/static_site/static", public_path="/home/zeriz/workspace/course_projects/static_site/public"):
+    if not os.path.exists(static_path):
+        raise Exception(f"{static_path} does not exist.")
     if os.path.exists(public_path):
         shutil.rmtree(public_path)
     os.mkdir(public_path)
-    path_list = os.listdir(path)
+    path_list = os.listdir(static_path)
     for path in path_list:
-        static = static + "/" + path
-        public = public + "/" + path
-        if not os.path.isfile(path):
-            os.mkdir(static)
-            os.mkdir(public)
-            static_to_public(static, public_path)
+        new_static_path = static_path + "/" + path
+        new_public_path = public_path + "/" + path
+        if not os.path.isfile(new_static_path):
+            static_to_public(new_static_path, new_public_path)
         else:
-            shutil.copy(path, public_path)
+            shutil.copy(new_static_path, new_public_path)
